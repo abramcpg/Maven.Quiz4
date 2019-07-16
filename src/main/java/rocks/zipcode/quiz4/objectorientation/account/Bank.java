1abd17731a8abb18235e30333e912158dbd733e9
Abram Phillips <abramcpg@hotmail.com>
2019-07-16 13:01:02 -0400

    87/154 tests passed

--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/Bank.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/Bank.java
@@ -1,17 +1,24 @@
 package rocks.zipcode.quiz4.objectorientation.account;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author leon on 27/12/2018.
  */
 public class Bank {
-    public BankAccount removeBankAccountByIndex(Integer indexNumber) {
-        return null;
+    private ArrayList<BankAccount> accountList = new ArrayList<BankAccount>();
+
+    public void removeBankAccountByIndex(Integer indexNumber) {
+        this.accountList.remove(indexNumber);
+        //return null;
     }
 
     public void addBankAccount(BankAccount bankAccount) {
+        this.accountList.add(bankAccount);
     }
 
     public Boolean containsBankAccount(BankAccount bankAccount) {
-        throw new UnsupportedOperationException("Method not yet implemented");
+        return this.accountList.contains(bankAccount);
     }
 }
--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/Employee.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/Employee.java
@@ -3,11 +3,18 @@ package rocks.zipcode.quiz4.objectorientation.account;
 /**
  * @author leon on 30/12/2018.
  */
-public class Employee {
+public class Employee implements Worker{
+    Double hourlyWage = null;
+    Double hoursWorked = null;
+    Double balance = null;
+    Double bankAccountBalance = balance;
+    Double moneyEarned = hourlyWage * hoursWorked;
     public Employee() {
+
     }
 
     public Employee(BankAccount bankAccount) {
+        Double balance;
     }
 
     public BankAccount getBankAccount() {
@@ -15,6 +22,25 @@ public class Employee {
     }
 
     public void setBankAccount(BankAccount bankAccount) {
+    }
 
+    @Override
+    public void increaseHoursWorked(Double numberOfHours) {
+        this.hoursWorked += numberOfHours;
+    }
+
+    @Override
+    public Double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    @Override
+    public Double getHourlyWage() {
+        return hourlyWage;
+    }
+
+    @Override
+    public Double getMoneyEarned() {
+        return moneyEarned;
     }
 }
