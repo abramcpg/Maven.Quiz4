c4877301fbae46d9784bf4f7151b2bfe547a3e0a
Abram Phillips <abramcpg@hotmail.com>
2019-07-16 09:32:19 -0400

    26/154 tests passed

--- src/main/java/rocks/zipcode/quiz4/fundamentals/Calculator.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/Calculator.java
@@ -1,19 +1,26 @@
 package rocks.zipcode.quiz4.fundamentals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author leon on 21/12/2018.
  */
 public class Calculator {
     public static Double squareRoot(Double value) {
-        return null;
+        return Math.sqrt(value);
     }
 
     public static Double square(Double value) {
-        return null;
+        return value*value;
     }
 
     public static Double[] squareRoots(Double... value) {
-        return null;
+        ArrayList<Double> array = new ArrayList(value.length);
+        for(Double i: value) array.add(Math.sqrt(i));
+        Double[] newArray = new Double[value.length];
+        newArray = (Double[]) array.toArray();
+        return newArray;
     }
 
     public static Double[] squares(Double... values) {
@@ -21,15 +28,15 @@ public class Calculator {
     }
 
     public static Double add(Double value1, Double value2) {
-        return null;
+        return value1+value2;
     }
 
     public static Double subtract(Double value1, Double value2) {
-        return null;
+        return value1-value2;
     }
 
 
     public static Double divide(Double divisor, Double dividend) {
-        return null;
+        return divisor/dividend;
     }
 }
